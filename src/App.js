3bf28558fd574c69fea7f48b1d5e9c6deef5caf2
Benoit Clavel <benoitclavel@yahoo.fr>
2019-05-02 20:27:59 +0200

    Ajout states & events

--- src/App.js
+++ src/App.js
@@ -10,31 +10,57 @@ import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
 
+  constructor(props) {
+  super(props);
+
+  this.handleClickLikeOn = this.handleClickLikeOn.bind(this);
+  this.handleClickLikeOff = this.handleClickLikeOff.bind(this);
+
+  this.state = {
+    viewOnlyLike : false
+  };
+}
+
+handleClickLikeOn() {
+  console.log('light lights ON');
+  this.setState({
+    viewOnlyLike : true
+  })
+}
+
+handleClickLikeOff() {
+  console.log('light lights OFF');
+  this.setState({
+    viewOnlyLike : false
+  })
+}
+
  render() {
+   console.log('log de this app', this);
 
    var moviesData = [{
      name: 'L\'Odyssée sans but de Kóstas Mítroglou',
      desc: 'Après que leur club ait été victime d\'une violente tempête au mercato, un adolescent et un attaquant grec en mal d\'efficacité …',
-     img: './images/pi.jpg'
+     img: './images/pi.jpg',
    },{
      name: 'Jean-Michel le maléfique',
      desc: 'Poussée par la vengeance et une volonté farouche de protéger son club de marde qu\'il préside, Jean-Michel place ...',
-     img: './images/malefique.jpg'
+     img: './images/malefique.jpg',
    },{
      name: 'Tintin au pays des gilets jaunes',
      desc: 'Après avoir regardé les infos sur le MediaTV, Tintin, un jeune reporter, se retrouve entraîné dans une bien triste aventure...',
-     img: './images/tintin.jpg'
+     img: './images/tintin.jpg',
    }]
 
   var moviesList = moviesData.map((movie, i) => {
-    return <Film movieName={movie.name} movieDesc={movie.desc} movieImg={movie.img} key={i} />
+    return <Film movieName={movie.name} movieDesc={movie.desc} movieImg={movie.img} key={i} displayOnlyLike={this.state.viewOnlyLike} />
   })
-
+  console.log('state de viewOnlyLike ->', this.state.viewOnlyLike);
   return (
     <div style={styles.background}>
        <Container>
         <Row style={styles.header}>
-          <Col><Header/></Col>
+          <Col><Header handleClickLikeOn={this.handleClickLikeOn} handleClickLikeOff={this.handleClickLikeOff} displayOnlyLike={this.state.viewOnlyLike} /></Col>
         </Row>
         <Row>
           {moviesList}
@@ -46,16 +72,16 @@ class App extends Component {
 }
 
 
-
 class Header extends Component {
   constructor(props) {
   super(props);
 
   this.toggle = this.toggle.bind(this);
   this.state = {
-    isOpen: false
+    isOpen: false,
   };
 }
+
 toggle() {
   this.setState({
     isOpen: !this.state.isOpen
@@ -65,8 +91,6 @@ toggle() {
    var moviesNameList = ['Titre1', 'Titre2', 'Titre3', 'Titre4', 'Titre5']
    var moviesLast = []
    var moviesCount = ''
-
-   console.log(moviesNameList);
 
    if (moviesNameList.length == 0) {
      moviesLast = 'Aucun film sélectionné'
@@ -82,7 +106,7 @@ toggle() {
      moviesCount = `${moviesNameList.length} films`
    }
 
-  return (
+    return (
     <div>
         <Navbar color="#161d23" light expand="md">
           <NavbarBrand href="/"><img style={styles.headerLogo} src="../images/logo.png"/></NavbarBrand>
@@ -90,14 +114,12 @@ toggle() {
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-1" navbar>
               <NavItem>
-                <NavLink href="#" style={styles.navLinks}>Last releases</NavLink>
+                <NavLink href="#" style={(this.props.displayOnlyLike) ? styles.navLinksOn : styles.navLinksOff} onClick={this.props.handleClickLikeOff}>Last releases</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="#" style={styles.navLinksMyMovies}>My Movies</NavLink>
+                <NavLink href="#" style={(this.props.displayOnlyLike) ? styles.navLinksOff : styles.navLinksOn} onClick={this.props.handleClickLikeOn}>My Movies</NavLink>
               </NavItem>
-              <Button id="UncontrolledPopover" type="button" style={styles.navLinksLikes}>
-                      {moviesCount}
-              </Button>
+              <Button id="UncontrolledPopover" type="button" style={styles.navLinksLikes}>{moviesCount}</Button>
               <UncontrolledPopover placement="bottom" target="UncontrolledPopover">
                 <PopoverHeader>Derniers films ajoutés</PopoverHeader>
                 <PopoverBody>{moviesLast}</PopoverBody>
@@ -112,12 +134,52 @@ toggle() {
 
 
 class Film extends Component {
+
+  constructor() {
+   super();
+   this.likeClick = this.likeClick.bind(this);
+   this.state = {
+     like : false,
+   }
+  }
+
+  likeClick(){
+   console.log("love click détécté");
+   if (!this.state.like) {
+     this.setState({
+       like : true
+     })
+   } else {
+     this.setState({
+       like : false
+   })
+  }
+}
+
+
+
  render() {
+
+   var displayFilm;
+   if (!this.props.displayOnlyLike) {
+     displayFilm = true
+   } else if (this.props.displayOnlyLike && this.state.like){
+     displayFilm = true
+   } else {
+     displayFilm = false
+   }
+
+   var heart
+   if (this.state.like) {
+     heart = <FontAwesomeIcon icon={faHeartSolid} style={styles.styleHeartActive} onClick={this.likeClick} />
+   } else {
+     heart = <FontAwesomeIcon icon={faHeart} style={styles.styleHeart} onClick={this.likeClick} />
+   }
   return (
-    <Col xs='12' sm='6' lg='3'>
+    <Col xs='12' sm='6' lg='3' style={{display : (displayFilm) ? 'block' : 'none'}}>
       <div style={styles.film}>
         <Card>
-          <FontAwesomeIcon icon={faHeart} style={styles.styleHeart} />
+          {heart}
           <CardImg top width="100%" src={this.props.movieImg} alt={this.props.movieName} />
           <CardBody style={styles.cardBody}>
             <CardTitle style={styles.filmTitle}>{this.props.movieName}</CardTitle>
@@ -131,25 +193,6 @@ class Film extends Component {
 }
 
 
-// class FilmHeart extends Component {
-//  render() {
-//   return (
-//     <div style={styles.film}>
-//       <Card>
-//         <FontAwesomeIcon icon={faHeartSolid} style={styles.styleHeartActive} />
-//         <CardImg top width="100%" src="../images/tintin.jpg" alt="Card image cap" />
-//         <CardBody style={styles.cardBody}>
-//           <CardTitle style={styles.filmTitle}>Tintin au pays des gilets jaunes</CardTitle>
-//           <CardSubtitle style={styles.filmText}>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</CardSubtitle>
-//         </CardBody>
-//       </Card>
-//     </div>
-//   );
-//  }
-// }
-
-
-
 var styles = {
   background : {
     backgroundColor : '#131A20',
@@ -161,11 +204,11 @@ var styles = {
   headerLogo : {
     width: 35,
   },
-  navLinks : {
+  navLinksOff : {
     color : '#ffffff',
     fontSize : 13
   },
-  navLinksMyMovies : {
+  navLinksOn : {
     color : '#6c757c',
     fontSize : 13
   },
